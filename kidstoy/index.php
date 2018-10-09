b9fe8c609f9d9712ce775f684112b72587006cf1
aimanatan27 <35244752+aimanatan27@users.noreply.github.com>
2018-10-09 14:54:53 +0800

    Add files via upload

---
+++ kidstoy/index.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once("config.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM barangmainan ORDER BY id ASC");
+?>
+
+<html>
+    <head>
+          <title>Senarai Barangan Kedai Dulu-Dulu</title>
+    </head>
+    <body>
+    <center>
+        <img src="logo.PNG">
+        <h2>SENARAI BARANGAN DAN HARGA</h2> 
+        <fieldset>
+            <table width='70%' border='1' padding='2' cellspacing='2'>
+                <tr bgcolor='#CCCCCC'>
+                    <td>Bil.</td>
+                    <td>Nama Produk</td>
+                    <td>Harga</td>
+                    <td>Tindakan</td>
+                </tr>
+
+<?php
+      $no=1;
+      
+      while($res = mysqli_fetch_array($result)) {
+          echo "<tr>";  
+          echo "<td>".$no; 
+          echo "<td>".$res['nama']."</td>"; 
+          echo "<td>".$res['harga']."</td>"; 
+          echo "<td><a href=\"delete.php?id=$res[id]\" onClick=\return confirm('Adakah anda pasti?')
+                    \">hapus</a></td>";
+          $no++;
+      }
+         ?>
+            </table>
+            <br><a href="add.php">Daftar Barang Baru</a>
+    </center>
+  </fieldset>
+    </body>
+</html>
+
